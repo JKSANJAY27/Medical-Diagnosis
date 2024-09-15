272ffc4bddddad313f1012d84af19402b748d1f3
SANJAY J K <142034123+JKSANJAY27@users.noreply.github.com>
2024-09-15 14:53:03 +0530

    Update dbConfig.jsx

--- src/utils/dbConfig.jsx
+++ src/utils/dbConfig.jsx
@@ -2,6 +2,6 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 const sql = neon(
-    "postgresql://neondb_owner:6kpD1nCfQHjR@ep-withered-mode-a1aum754.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
+    "<include PostgreSQL database link here>"
 );
 export const db = drizzle(sql, { schema });
