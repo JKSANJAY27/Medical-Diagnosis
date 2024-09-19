d356440685ae1e9ce46a1aa3642aaad3b7df74de
SANJAY J K <142034123+JKSANJAY27@users.noreply.github.com>
2024-09-19 22:51:59 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,12 +3,12 @@ import { Link, useNavigate } from "react-router-dom";
 
 import { sun } from "../assets";
 import { navlinks } from "../constants";
-import { IconHeartHandshake } from "@tabler/icons-react";
+import { IconReportMedical } from "@tabler/icons-react";
 
 const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
   <div
     className={`h-[48px] w-[48px] rounded-[10px] ${
-      isActive && isActive === name && "bg-[#2c2f32]"
+      isActive && isActive === name && "bg-[#43ad96]"
     } flex items-center justify-center ${
       !disabled && "cursor-pointer"
     } ${styles}`}
@@ -33,12 +33,12 @@ const Sidebar = () => {
   return (
     <div className="sticky top-5 flex h-[93vh] flex-col items-center justify-between">
       <Link to="/">
-        <div className="rounded-[10px] bg-[#2c2f32] p-2">
-          <IconHeartHandshake size={40} color="#1ec070" className=" " />
+        <div className="rounded-[10px] bg-[#4b7ad3] p-2">
+          <IconReportMedical stroke={2} size={40} color="#ffffff" className=" " />
         </div>
       </Link>
 
-      <div className="mt-12 flex w-[76px] flex-1 flex-col items-center justify-between rounded-[20px] bg-[#1c1c24] py-4">
+      <div className="mt-12 flex w-[76px] flex-1 flex-col items-center justify-between rounded-[20px] bg-[#14558acc] py-4">
         <div className="flex flex-col items-center justify-center gap-3">
           {navlinks.map((link) => (
             <Icon
@@ -55,7 +55,7 @@ const Sidebar = () => {
           ))}
         </div>
 
-        <Icon styles="bg-[#1c1c24] shadow-secondary" imgUrl={sun} />
+        <Icon styles="bg-[#43ad96] shadow-secondary" imgUrl={sun} />
       </div>
     </div>
   );
